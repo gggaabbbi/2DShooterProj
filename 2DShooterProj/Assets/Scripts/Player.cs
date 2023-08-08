e8ede2a431fcf7706308f2b514bc59dce7d26296
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2023-08-08 20:54:11 -0300

    Topdown Movement

--- 2DShooterProj/Assets/Scripts/Player.cs
+++ 2DShooterProj/Assets/Scripts/Player.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    private Animator animator;
+    private Transform playerTransform;
+
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        playerTransform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        playerTransform.Translate(moveX, moveY, 0);
+    }
+    
 }
